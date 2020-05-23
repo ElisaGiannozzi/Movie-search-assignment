1d7b2d5f4447dd0499ac7cc6c528fcbfc6397820
ElisaGiannozzi <elisa.giannozzi@gmail.com>
2020-05-23 15:39:03 +0200

    styled search bar and result container

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 $("#search").on('click', function() { 
   const title = $('#movie-name').val();         
   $.ajax('http://www.omdbapi.com/?apikey=7722430f&t='+title).done(function(response){                   
-        let container = $("#result-container");
+        let container = $("#text-container");
+        let imgContainer = $('#img-container'); 
+        $('#text-container').empty(); 
+        $('#img-container').empty(); 
+        $('#movie-name').keydown(function(e){
+          if(e.which === 13){
+              $("#search").click();
+          }
+        });
         if (response.Response === "False") {
           alert(response.Error);
         } else {
         for (let key in response) {
           if (key === 'Title') {
-            container.append(`<div>${key} ${response[key]}</div>`); 
+            container.append(`<div>${key}: ${response[key]}</div>`); 
           }
           if (key === 'Actors') {
-            container.append(`<div>${key} ${response[key]}</div>`); 
+            container.append(`<div>${key}: ${response[key]}</div>`); 
           }
           if (key === 'Awards') {
-            container.append(`<div>${key} ${response[key]}</div>`); 
+            container.append(`<div>${key}: ${response[key]}</div>`); 
+          }
+          if (key === 'Genre') {
+            container.append(`<div>${key}: ${response[key]}</div>`); 
+          }
+          if (key === 'Language') {
+            container.append(`<div>${key}: ${response[key]}</div>`); 
           }
           if (key === 'Poster') {
-            container.append(`<div><img src="${response[key]}"></div>`); 
+            imgContainer.append(`<div><img src="${response[key]}"></div>`); 
           }
         }
       }
     });
 });
+
 
 
 
